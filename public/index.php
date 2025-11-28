ae1ec5256a1c3a01ce308e68b94827ecc71ee61e
agservizi <ag.servizi16@gmail.com>
2025-11-28 06:39:01 +0100

    Force PHP error logging to storage

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+ini_set('log_errors', '1');
+ini_set('error_log', dirname(__DIR__) . '/storage/logs/php-error.log');
 
 require_once __DIR__ . '/../config/config.php';
 
